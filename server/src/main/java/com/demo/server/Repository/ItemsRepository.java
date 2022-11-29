3fe3b6730976796b6245b8a82a916636120b26b2
Siri2K <kanagalingham.sriram@gmail.com>
2022-11-28 20:48:13 -0500

    Fixed Syntax Errors on User Login and Logout

--- server/src/main/java/com/demo/server/Repository/ItemsRepository.java
+++ server/src/main/java/com/demo/server/Repository/ItemsRepository.java
@@ -1,12 +1,14 @@
-package com.demo.server.Repository;
+package com.demo.server.repository;
 
 // Import Model
 import com.demo.server.model.Items;
 
-// Import Java Packages
+// Import Java and Spring Packages
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Setup Data Access
+@Repository
 public interface ItemsRepository extends JpaRepository<Items,Integer>
 {
     
