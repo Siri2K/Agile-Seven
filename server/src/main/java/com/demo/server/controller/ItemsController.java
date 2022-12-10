573ca335fdc6108b6fa4c944c944171f50ecdc72
Siri2K <kanagalingham.sriram@gmail.com>
2022-12-10 10:44:31 -0500

    Made Item Connections to Front-end

--- server/src/main/java/com/demo/server/controller/ItemsController.java
+++ server/src/main/java/com/demo/server/controller/ItemsController.java
@@ -4,22 +4,28 @@ package com.demo.server.controller;
 import com.demo.server.model.Items;
 import com.demo.server.repository.ItemsRepository;
 
-import java.io.IOException;
 // Imports Spring and Java Classes
 import java.util.Optional;
+import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,32 +41,74 @@ public class ItemsController
         this.itemsRepository = itemsRepository;
     }
 
-    @GetMapping(path = "/items")
-    public @ResponseBody List<Items> listItems() 
+    @GetMapping("/")
+    public String listItems(Model model)
     {
-        return itemsRepository.findAll();
+        model.addAttribute("allItems", itemsRepository.findAll());
+        return "index";
     }
 
-    @GetMapping(path = "/items/{id}")
-    public @ResponseBody Optional<Items> findItems(@PathVariable("id") Integer id) 
+    @GetMapping("/add")
+    public String addItem(Model model)
     {
-        return itemsRepository.findById(id);
+        Items item = new Items();
+        model.addAttribute("item", item);
+        return "newItem";
     }
 
-    
-    @PostMapping(path = "/items")
-    public @ResponseBody void addItem(@RequestBody Items item) 
+    @PostMapping("/save")
+    public String saveItem(@ModelAttribute("item") Items item)
     {
         itemsRepository.save(item);
+        return "redirect:/";
     }
 
-    @DeleteMapping(path = "/items/{id}")
-    public @ResponseBody void deleteItem(@RequestParam("id") int id) 
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable(value = "id") Integer id)
     {
         itemsRepository.deleteById(id);
+        return "redirect:/";
     }
-    
 
+    @GetMapping("/{id}")
+	public void showItemsImage(@PathVariable("id") Integer Id, HttpServletResponse response,Optional<Items> item) 
+    throws ServletException, IOException
+    {
+        item = itemsRepository.findById(Id);
+        response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+        response.getOutputStream().write(item.get().getPicture());
+        response.getOutputStream().close();
+    }
+
+    /* */
+    @GetMapping("/name_asc")
+    public String listItemsByNameAsc(Model model)
+    {
+        // Choose Sort Location
+        Sort sort = Sort.by("name").ascending();
+        model.addAttribute("allItems", itemsRepository.findAll(sort));
+        return "redirect:/";
+    }
+
+    @GetMapping("/price_asc")
+    public String listItemsByPriceAsc(Model model)
+    {
+        // Choose Sort Location
+        Sort sort = Sort.by("name").ascending();
+        model.addAttribute("allItems", itemsRepository.findAll(sort));
+        return "redirect:/";
+    }
+
+    @GetMapping("/price_desc")
+    public String listItemsByPriceDesc(Model model)
+    {
+        // Choose Sort Location
+        Sort sort = Sort.by("name").descending();
+        model.addAttribute("allItems", itemsRepository.findAll(sort));
+        return "redirect:/";
+    }
+
+    /*   
     @GetMapping("items/image/{id}")
 	public void showItemsImage(@PathVariable("id") Integer Id, HttpServletResponse response,Optional<Items> item) 
     throws ServletException, IOException
@@ -104,5 +152,6 @@ public class ItemsController
         Sort sort = Sort.by("price").descending();
         return itemsRepository.findAll(sort);
     }
+    */
     
 }
